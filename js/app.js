2244d6f2a51b201218753e025ed6bb1fd4e9297b
unknown <saparbekov.ilzar@bk.rugit config --global user.name>
2023-01-20 15:33:00 +0600

    DZ

---
+++ js/app.js
@@ -0,0 +1,77 @@
+const tabs = document.querySelectorAll('.tabheader__item')
+const tabsParent = document.querySelector('.tabheader__items')
+const tabContent = document.querySelectorAll('.tabcontent')
+
+const hideTabContent = () => {
+    tabContent.forEach(item => {
+        item.style.display = 'none'
+    })
+    tabs.forEach(item => {
+        item.classList.remove('tabheader__item_active')
+    })
+}
+
+const showTabContent = (i = 0) => {
+    tabContent[i].style.display = 'block'
+    tabs[i].classList.add('tabheader__item_active')
+}
+
+hideTabContent()
+showTabContent()
+//1
+let count = 0
+function autoSlider() {
+    const slider = setInterval(()=> {
+        count++
+        if (count > 3){
+            count = 0
+        }
+        hideTabContent()
+        showTabContent(count)
+    }, 2000)
+
+}
+autoSlider()
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(i)
+                count = i
+            }
+        })
+    }
+}
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('.btn_white')
+const closeModalBtn = document.querySelector('.modal__close')
+
+const openModal = () => {
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+    document.body.style.overflow = 'hidden'
+}
+
+const closeModal = () => {
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+    document.body.style.overflow = ''
+}
+
+modalTrigger.addEventListener('click', openModal)
+closeModalBtn.addEventListener('click', closeModal)
+
+modal.onclick = (event) => event.target === modal ? closeModal() : false
+window.onkeydown = (event) => event.code === 'Backspace' ? closeModal() : false
+
+//2
+const scroll = ()=> {
+    const footer = document.documentElement;
+    if (footer.scrollTop + footer.clientHeight >= footer.scrollHeight){
+        openModal()
+    }
+}
+window.addEventListener('scroll', scroll)
